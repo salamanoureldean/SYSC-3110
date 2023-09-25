aa6322fdb1944dc7115d61540cae89b753be2ffc
salama <salamanoureldean2001@gmail.com>
2023-09-25 13:17:38 -0400

    Changed the index being removed.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,30 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class AddressBook {
-    private List<BuddyInfo> buddyList;
+public class AddressBook{
+    private ArrayList<BuddyInfo> buddyList;
 
-    public AddressBook() {
+    public AddressBook(){
         buddyList = new ArrayList<>();
     }
 
     public void addBuddy(BuddyInfo buddy) {
-        buddyList.add(buddy);
+        if (buddy != null) {
+            buddyList.add(buddy);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy) {
-        buddyList.remove(buddy);
+    public BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < buddyList.size()){
+            return buddyList.remove(index);
+        }
+        return null;
     }
 
-    public List<BuddyInfo> getBuddyList() {
+    public List<BuddyInfo> getBuddyList(){
         return buddyList;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Address Book");
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(1);
     }
 }
